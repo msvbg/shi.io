f90920809645199279d85e946898d14b704ce3c4
Martin Svanberg <martin@martinsvanberg.com>
2014-12-07 23:33:16 -0800

    Add basic build Gulpfile for app.

---
+++ Gulpfile.js
@@ -0,0 +1,88 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    autoprefix = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    browserify = require('browserify'),
+    es6ify = require('es6ify'),
+    source = require('vinyl-source-stream'),
+    livereload = require('gulp-livereload'),
+    concat = require('gulp-concat'),
+    reactify = require('reactify');
+
+var paths = {
+    build: './build',
+
+    index: './app/index.html',
+    mainScript: './app/scripts/main.jsx',
+    mainStyle: './app/styles/main.scss',
+
+    styles: './app/**/*.scss'
+    scripts: './app/**/*.jsx',
+};
+
+var continueOnError = function (err) {
+    gutil.beep();
+    gutil.log(err);
+    this.emit("end");
+};
+
+gulp.task('buildScripts', ['buildVendorScripts'], function () {
+    return (
+        browserify({
+            files: es6ify.runtime,
+            debug: true
+        })
+        .require('./bower_components/react/react.js', {expose: 'react.js'})
+        .add(paths.mainScript)
+        .transform(reactify)
+        .transform(es6ify.configure(/.jsx/))
+        .bundle().on('error', continueOnError)
+        .pipe(source('scripts/app.js'))
+        .pipe(gulp.dest(paths.build)));
+});
+
+gulp.task('buildStyles', function () {
+    return gulp.src(paths.mainStyle)
+        .pipe(sass({
+            errLogToConsole: true,
+            includePaths: ['./bower_components/']
+        }))
+        .pipe(autoprefix())
+        .pipe(rename(function (path) {
+            path.dirname += '/styles';
+        }))
+        .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('buildIndex', function () {
+    return gulp.src(paths.index)
+        .pipe(gulp.dest(paths.build));
+})
+
+gulp.task('default', [
+    'buildIndex',
+    'buildScripts',
+    'buildStyles'], function () {
+
+    livereload.listen();
+
+    var watch = function (what) {
+        what.forEach(function (each) {
+            gulp.watch(each.dir, [each.action])
+                .on('change', function () {
+                    // Icky icky hack to compensate for slow JS build.
+                    setTimeout(livereload.changed, 100);
+                });
+        });
+    };
+  
+    watch([
+        { dir: paths.index, action: 'buildIndex' },
+        { dir: paths.scripts, action: 'buildScripts' },
+        { dir: paths.views, action: 'buildViews' },
+        { dir: paths.styles, action: 'buildStyles' },
+        { dir: paths.images, action: 'buildImages' }
+    ]);
+
+});
