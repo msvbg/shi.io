63ee29c3cdf708ec0e9d7e4884b2b972cbea43bf
Martin Svanberg <martin@martinsvanberg.com>
2015-01-01 16:50:34 -0800

    Flesh out server, add minimal search interface

---
+++ app/server/api.js
@@ -0,0 +1,31 @@
+let pg = require('pg');
+
+let connectionString = 'postgres://martin:@localhost/shi_io';
+
+function queryDatabase(query, parameters = []) {
+    console.log('Querying', query);
+
+    return new Promise(function (resolve, reject) {
+        pg.connect(connectionString, function (error, client, done) {
+            if (error) { reject(error); }
+
+            client.query(query, parameters, function (error, result) {
+                done();
+                if (error) { reject(error); }
+
+                resolve(result);
+            });
+        });
+    });
+}
+
+
+export function search (query) {
+    return queryDatabase(
+        `SELECT
+            headword
+        FROM entry
+        WHERE headword LIKE %$1%`,
+        [query])
+    .then((result) => result.rows);
+}
--- app/server/main.js
+++ app/server/main.js
@@ -1,8 +1,17 @@
+import {search} from 'api';
+
 export function run (appdir) {
     let express = require('express');
+    let compression = require('compression');
     let app = express();
 
     app.use(express.static(appdir + '/../../public'));
+    app.use(compression());
+
+    app.get('/api/search', function (req, res) {
+        let searchQuery = req.query['query'];
+        res.sendStatus(search(searchQuery));
+    });
 
     let server = app.listen(1534, function () {
         console.log('Listening on 1534');
